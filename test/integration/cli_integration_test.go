b8156fb3c663770ac1a90fa2eb681582cdf1667f
kompl3xpr <kompl3xpr@proton.me>
2025-10-03 23:48:47 +0800

    style: update sources

--- test/integration/cli_integration_test.go
+++ test/integration/cli_integration_test.go
@@ -1,14 +1,11 @@
 /*
- * ░█░█░█▀█░▀▀█░█▀█░█▀▄░▀█▀░█▀█░
- * ░░█░░█░█░▄▀░░█░█░█▀▄░░█░░█░█░
- * ░░▀░░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀▀▀░▀░▀░
  * Copyright (c) 2025 kompl3xpr. All rights reserved.
  * Use of this source code is governed by a MIT license
  * that can be found in the LICENSE file.
  *
  * File:               test/integration/cli_integration_test.go
  * Last modified by:   kompl3xpr <kompl3xpr@proton.me>
- * Last modified time: 2025-10-01 09:13:46 UTC
+ * Last modified time: 2025-10-03 15:48:48 UTC
  */
 
 package integration
